a22f21c83e7468140840c8a7d067fa38cb402138
Christian Hensen <christian.j.hensen@gmail.com>
2017-07-23 18:52:44 -0400

    added jquery lib and commented app.js with what I need to do

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Add items to list
+  // Type in text box
+  // Add that item to template code
+  // Append template code to container div
+
+// Check
+  // Get title for item
+  // Change styling on item
+    // Strikethrough
+
+// Delete item from list
+  // Get the parent node for the current item
+  // Remove node
