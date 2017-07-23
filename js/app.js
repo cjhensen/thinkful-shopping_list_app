70f151a13257504af2e4a896063f9a35c82c3f95
Christian Hensen <christian.j.hensen@gmail.com>
2017-07-23 19:24:16 -0400

    implemented all app functions including add, check, and delete

--- js/app.js
+++ js/app.js
@@ -11,3 +11,60 @@
 // Delete item from list
   // Get the parent node for the current item
   // Remove node
+
+$(function(){
+
+// Add
+
+const shoppingForm = $('#js-shopping-list-form');
+const shoppingFormInput = shoppingForm.find('.js-shopping-list-entry');
+const shoppingListContainer = $('.shopping-list');
+
+function getInputValue() {
+  console.log(shoppingFormInput.val());
+  return shoppingFormInput.val();
+}
+
+function clearInputValue() {
+  shoppingFormInput.val("");
+}
+
+function addToShoppingList(inputValue) {
+
+  shoppingListContainer.append(
+    `<li>
+        <span class="shopping-item">${inputValue}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
+    );
+}
+
+shoppingForm.submit(function() {
+  event.preventDefault();
+
+  getInputValue();
+
+  addToShoppingList(getInputValue());
+
+  clearInputValue();
+});
+
+// Check
+shoppingListContainer.on('click', 'li .shopping-item-toggle', function() {
+  $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+});
+
+// Delete
+shoppingListContainer.on('click', 'li .shopping-item-delete', function() {
+  $(this).closest('li').remove();
+});
+
+
+});
